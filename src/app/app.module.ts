8f6d9eac63c8cb98a3137759b87231bc7171ae62
haze272 <studiorain27@gmail.com>
2022-11-01 08:49:41 +0300

    Добавление item компонента
    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { ProductItemComponent } from './components/shared/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
