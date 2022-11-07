21216fcecda25df93957b95622e9c371dbf5e4e2
haze272 <studiorain27@gmail.com>
2022-11-07 05:09:48 +0300

    Рефакторинг

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,26 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductService } from 'src/app/services/product-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  providers: [HttpClient]
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private httpClient: HttpClient) {
-     //this.products = productService.getProducts();
-     this.httpClient.get<Product[]>('https://pet-esteban.ru/ited-api/marketplace/users/esteban/data')
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts()
      .subscribe((res) => {
       this.products = res;
      })
   }
-
-  ngOnInit(): void {
-  }
-
 }
