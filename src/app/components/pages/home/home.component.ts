4e50426a78082a26e63d6111bbbd4be13cf7d86b
haze272 <studiorain27@gmail.com>
2022-11-08 09:39:30 +0300

    Стилизация домашней страницы

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -8,6 +8,11 @@ import { Product, ProductService } from 'src/app/services/product-service';
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
+  taco: string = 'assets/img/taco.png';
+  hombre: string = 'assets/img/hombre.png';
+  burrito: string = 'assets/img/burrito.png';
+  nachos: string = 'assets/img/nachos.png';
+  mescalina: string = 'assets/img/mescalina.png';;
 
   constructor(private productService: ProductService) {}
 
